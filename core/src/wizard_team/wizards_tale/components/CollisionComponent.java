6edd74f2e8e266af33f098afa65cd223198df7d3
Daniel Lisø <daniel.lisoe@gmail.com>
2018-04-10 14:04:27 +0200

    Add collision detection and handling

---
+++ core/src/wizard_team/wizards_tale/components/CollisionComponent.java
@@ -0,0 +1,6 @@
+package wizard_team.wizards_tale.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Rectangle;
+
+public class CollisionComponent implements Component {}
--- core/src/wizard_team/wizards_tale/systems/VelocityMovementSystem.java
+++ core/src/wizard_team/wizards_tale/systems/VelocityMovementSystem.java
@@ -2,16 +2,25 @@ package wizard_team.wizards_tale.systems;
 
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.systems.IteratingSystem;
+
+import wizard_team.wizards_tale.components.BoundRectComponent;
+import wizard_team.wizards_tale.components.CollisionComponent;
 import wizard_team.wizards_tale.components.PositionComponent;
 import wizard_team.wizards_tale.components.VelocityComponent;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.math.Rectangle;
 
 public class VelocityMovementSystem extends IteratingSystem {
   private ComponentMapper<PositionComponent> posMapper =
       ComponentMapper.getFor(PositionComponent.class);
+
   private ComponentMapper<VelocityComponent> velMapper =
       ComponentMapper.getFor(VelocityComponent.class);
+
+  private ComponentMapper<BoundRectComponent> boundMapper =
+      ComponentMapper.getFor(BoundRectComponent.class);
 
   public VelocityMovementSystem() {
     super(Family.all(PositionComponent.class, VelocityComponent.class).get());
@@ -21,7 +30,27 @@ public class VelocityMovementSystem extends IteratingSystem {
     PositionComponent pos = posMapper.get(e);
     VelocityComponent vel = velMapper.get(e);
 
+    ImmutableArray<Entity> collidables = getEngine().getEntitiesFor(
+            Family.all(BoundRectComponent.class, PositionComponent.class).get()
+    );
+
     pos.x += vel.v_x * dt;
     pos.y += vel.v_y * dt;
+
+    if(boundMapper.has(e)) {
+      BoundRectComponent bound = boundMapper.get(e);
+      bound.boundRect.setPosition(pos.x, pos.y);
+      for(Entity other : collidables) {
+        if(e != other) {
+            BoundRectComponent otherBound = boundMapper.get(other);
+            if(bound.boundRect.overlaps(otherBound.boundRect)) {
+              pos.x -= vel.v_x * dt;
+              pos.y -= vel.v_y * dt;
+              bound.boundRect.setPosition(pos.x, pos.y);
+            }
+        }
+      }
+    }
+
   }
 }
