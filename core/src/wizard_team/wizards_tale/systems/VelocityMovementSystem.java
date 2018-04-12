7a778f38a62e243f77b84c241470b06e6d3afa26
Daniel Lisø <daniel.lisoe@gmail.com>
2018-04-12 15:08:30 +0200

    Let players move through everything

--- core/src/wizard_team/wizards_tale/systems/VelocityMovementSystem.java
+++ core/src/wizard_team/wizards_tale/systems/VelocityMovementSystem.java
@@ -2,6 +2,8 @@ package wizard_team.wizards_tale.systems;
 
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.systems.IteratingSystem;
+import wizard_team.wizards_tale.components.ReceiveInputComponent;
+import com.badlogic.ashley.core.Component;
 
 import wizard_team.wizards_tale.components.BoundRectComponent;
 import wizard_team.wizards_tale.components.CollisionComponent;
@@ -26,31 +28,42 @@ public class VelocityMovementSystem extends IteratingSystem {
     super(Family.all(PositionComponent.class, VelocityComponent.class).get());
   }
 
+  static boolean cantPassThrough(Entity e1, Entity e2) {
+    boolean x = true;
+    for (Component c : e1.getComponents()) {
+      if (c instanceof ReceiveInputComponent) {
+        x = false;
+      }
+    }
+    return x;
+  }
+
   public void processEntity(Entity e, float dt) {
     PositionComponent pos = posMapper.get(e);
     VelocityComponent vel = velMapper.get(e);
 
-    ImmutableArray<Entity> collidables = getEngine().getEntitiesFor(
-            Family.all(BoundRectComponent.class, PositionComponent.class).get()
-    );
+    ImmutableArray<Entity> collidables =
+        getEngine()
+            .getEntitiesFor(Family.all(BoundRectComponent.class, PositionComponent.class).get());
 
+    // Do the move.
     pos.x += vel.v_x * dt;
     pos.y += vel.v_y * dt;
 
-    if(boundMapper.has(e)) {
+    // Check if there's a collision. If there is, undo the move.
+    if (boundMapper.has(e)) {
       BoundRectComponent bound = boundMapper.get(e);
       bound.boundRect.setPosition(pos.x, pos.y);
-      for(Entity other : collidables) {
-        if(e != other) {
-            BoundRectComponent otherBound = boundMapper.get(other);
-            if(bound.boundRect.overlaps(otherBound.boundRect)) {
-              pos.x -= vel.v_x * dt;
-              pos.y -= vel.v_y * dt;
-              bound.boundRect.setPosition(pos.x, pos.y);
-            }
+      for (Entity other : collidables) {
+        if (e != other && cantPassThrough(e, other)) {
+          BoundRectComponent otherBound = boundMapper.get(other);
+          if (bound.boundRect.overlaps(otherBound.boundRect)) {
+            pos.x -= vel.v_x * dt;
+            pos.y -= vel.v_y * dt;
+            bound.boundRect.setPosition(pos.x, pos.y);
+          }
         }
       }
     }
-
   }
 }
