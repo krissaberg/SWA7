23d5c62a2dfaf94abb8d2d580504aa77cadb403f
Synnøve Halle <soonhalle@gmail.com>
2018-04-15 13:59:48 +0200

    Move constants into file and refactored

--- core/src/wizard_team/wizards_tale/components/constants/Constants.java
+++ core/src/wizard_team/wizards_tale/components/constants/Constants.java
@@ -8,10 +8,31 @@ public class Constants {
     public static final int DEFAULT_SPREADABLE_DEPTH = 3;
     public static final int PLAYER_START_X = 100;
     public static final int PLAYER_START_Y = 200;
-    private static final float CELL_HEIGHT = 100f;
-    private static final float CELL_WIDTH = 100f;
+    public static final float CELL_HEIGHT = 100f;
+    public static final float CELL_WIDTH = 100f;
+
+    //This is a testing dimension, but allows for a 4 hard blocks
+    public static final int MAP_X = 5;
+    public static final int MAP_Y = 5;
+
+    public static final String MAP_0 =
+            "NNNNN\n" +
+            "NHNHN\n" +
+            "NNNNN\n" +
+            "NHNHN\n" +
+            "NNNNN";
+
+    //TODO: for testing only, remove
+    public static final int SCREEN_X  = 800;
+    public static final int SCREEN_Y = 600;
 
     public enum EffectTypes {SPREAD, VANISH};
+
+    public enum CollidableType {
+        HARD, // Nothing can pass through this
+        SOFT  // Soft entities can pass through other soft entities
+    }
+
 
     public enum TimeConstants {
 
---
+++ core/src/wizard_team/wizards_tale/systems/DebugRenderSystem.java
@@ -0,0 +1,56 @@
+package wizard_team.wizards_tale.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
+import wizard_team.wizards_tale.components.BoundRectComponent;
+import wizard_team.wizards_tale.components.PositionComponent;
+import wizard_team.wizards_tale.components.SpriteComponent;
+
+public class DebugRenderSystem extends IteratingSystem {
+  private ComponentMapper<PositionComponent> posMapper =
+      ComponentMapper.getFor(PositionComponent.class);
+
+  private ComponentMapper<SpriteComponent> spriteMapper =
+      ComponentMapper.getFor(SpriteComponent.class);
+
+  private ComponentMapper<BoundRectComponent> boundMapper =
+          ComponentMapper.getFor(BoundRectComponent.class);
+
+  private SpriteBatch batch;
+  private ShapeRenderer shapeRenderer;
+
+  public DebugRenderSystem(SpriteBatch batch) {
+    super(Family.all(PositionComponent.class, SpriteComponent.class).get());
+    this.batch = batch;
+    shapeRenderer = new ShapeRenderer();
+  }
+
+  public void processEntity(Entity e, float dt) {
+    PositionComponent pos = posMapper.get(e);
+    SpriteComponent spriteComponent = spriteMapper.get(e);
+
+    Sprite sprite = spriteComponent.sprite;
+
+    sprite.setX(pos.x);
+    sprite.setY(pos.y);
+    batch.begin();
+    batch.draw(sprite.getTexture(), pos.x, pos.y);
+    batch.end();
+    if(boundMapper.has(e)) {
+      BoundRectComponent bounds = boundMapper.get(e);
+      Rectangle rect = bounds.boundRect;
+      shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+      shapeRenderer.setTransformMatrix(batch.getTransformMatrix());
+      shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+      shapeRenderer.rect(pos.x, pos.y, rect.width, rect.height);
+      shapeRenderer.end();
+    }
+  }
+}
